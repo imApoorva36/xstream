29aec624b08b6b33531d889a08aa2feb7b2e9523
Apoorva Agrawal <apoorva3569@gmail.com>
2025-10-15 10:51:42 +0530

    [Frontend]: Add configs

--- web-app/next.config.ts
+++ web-app/next.config.ts
@@ -1,9 +1,16 @@
-/** @type {import('next').NextConfig} */
-   const nextConfig = {
-     webpack: (config) => {
-       config.cache = false;
-       return config;
-     },
-   };
-   
-   module.exports = nextConfig;
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+
+    return config;
+  },
+};
+
+export default nextConfig;
